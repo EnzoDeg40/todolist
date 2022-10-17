0ba23e4e59bf47ec9e14c2b38f45648fc6627e9a
lucasporrini <2608lucas@gmail.com>
2022-10-17 16:45:47 +0200

    securite

--- checked.js
+++ checked.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    var list = document.querySelectorAll('li');
-
-    list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'li') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
+    var input = document.querySelector('.new-todo');
+    
+    document.addEventListener('keypress', function(e) {
+        if(e.keyCode === 13){
+            var list = document.querySelectorAll('.todo-list-item');
+            
+            list.forEach(e => {
+                e.addEventListener('click', ()=>{
+                    console.log(list);
+                    console.log(e.className);
+                    console.log(e.classList.contains("checked"));
+                    if(e.classList.contains("checked")){
+                            e.classList.remove('checked');
+                        }else{
+                            e.classList.add('checked');
+                    }
+                })
+            });
+        }
+    }) 
 })
