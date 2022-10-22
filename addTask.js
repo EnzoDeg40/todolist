edb61e694772b8ba21cf27b8cf2fd2d804d9c7b3
Francois Ly <ly.fongfrancois@gmail.com>
2022-10-22 21:22:51 +0200

    création de bouton tache en cours et de fin de tache

--- addTask.js
+++ addTask.js
@@ -1,9 +1,38 @@
 function addTask(task){
-    var taskList = document.querySelector('.todo-list');
-    var taskItem = document.createElement("li");
-    taskItem.innerHTML = task;
-    taskList.appendChild(taskItem);
 
+    
+
+    // création d'une div
+    var taskList = document.querySelector('.todo-list');
+    var defineDiv = document.createElement ("div")
+    defineDiv.classList.add("divTask")
+    taskList.appendChild(defineDiv)
+    //création li
+    var taskItem = document.createElement("li");
+    taskItem.classList.add("taskItem")
+    console.log( taskItem)
+    taskItem.innerHTML = task;
+    defineDiv.appendChild(taskItem);
+    
+    // création bouton tâche en cours
+    var progressTask = document.createElement('button')
+    progressTask.classList.add("btn-progress")
+    progressTask.innerHTML = "<i class='bi bi-tools'></i>";
+    defineDiv.appendChild(progressTask)
+    
+    // création bouton fin de tâche
+    var completedTask = document.createElement('button')
+    completedTask.classList.add("btn-completed")
+    completedTask.innerHTML = '<i class="bi bi-hand-thumbs-up"></i>';
+    defineDiv.appendChild(completedTask)
+
+    // //création bouton supprimé une tâche
+    // var deleteTask = document.createElement("button")
+    // deleteTask.classList.add("btn-trash")
+    // deleteTask.innerHTML='<i class="bi bi-trash"></i>'
+    // defineDiv.appendChild(deleteTask)
+
+    
     // Vide le champ de saisie
     document.querySelector('.new-todo').value = "";
 
@@ -22,6 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if(task){
                 addTask(task);
+               
+
             }
         }
     });
