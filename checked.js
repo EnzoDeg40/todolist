aa54c0356eaef5f4233fd5334a12ddeb6068311c
Enzo Degraeve <enzodeg40@gmail.com>
2022-12-12 11:18:59 +0100

    Correction barrage dans la liste au chargement de la page

--- checked.js
+++ checked.js
@@ -1,21 +1,26 @@
+function updateAddListener(){
+    let list = document.querySelectorAll('.todo-list-item');
+    list.forEach(e => {
+        // Remove event listener to prevent multiple event listeners
+        e.removeEventListener('click', ()=>{});
+
+        e.addEventListener('click', () => {
+            if(e.classList.contains("checked")){
+                e.classList.remove('checked');
+            }
+            else{
+                e.classList.add('checked');
+            }
+        });
+    });
+}
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    var input = document.querySelector('.new-todo');
+    let input = document.querySelector('.new-todo');
     
     input.addEventListener('keypress', function(e) {
         if(e.keyCode === 13){
-            var list = document.querySelectorAll('.todo-list-item');
-            list.forEach(e => {
-                // Remove event listener to prevent multiple event listeners
-                e.removeEventListener('click', ()=>{});
-
-                e.addEventListener('click', () => {
-                    if(e.classList.contains("checked")){
-                            e.classList.remove('checked');
-                        }else{
-                            e.classList.add('checked');
-                    }
-                })
-            });
+            updateAddListener();
         }
-    }) 
+    });
 })
